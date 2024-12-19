318483babcbc00f7cba6eb360c6313f1f1a1dbe4
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-12-19 19:46:14 +0200

    [~] Sync with [me_Uart].

--- examples/me_Bits/me_Bits.ino
+++ examples/me_Bits/me_Bits.ino
@@ -2,18 +2,18 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-10-25
+  Last mod.: 2024-12-19
 */
 
 #include <me_Bits.h>
 
 #include <me_BaseTypes.h>
-#include <me_UartSpeeds.h>
+#include <me_Uart.h>
 #include <me_Console.h>
 
 void setup()
 {
-  Serial.begin(me_UartSpeeds::Arduino_Normal_Bps);
+  me_Uart::Init(me_Uart::Speed_115k_Bps);
   Console.Print("[me_Bits] Start.");
   RunTest();
   Console.Print("[me_Bits] Done.");
