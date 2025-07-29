8917a1c281f7cb2b6c028ad9744604e449da13d5
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-07-29 14:42:19 +0200

    [/] [Interface] Interface change..
    Idea is to make low-level functions use as low logic as possible.

--- src/me_Bits.cpp
+++ src/me_Bits.cpp
@@ -11,72 +11,102 @@
 
 using namespace me_Bits;
 
-const TUint_1 BitsInByte = 8;
-const TUint_1 MaxOffset = BitsInByte - 1;
+const TUint_1
+  BitsInByte = 8,
+  MaxOffset = BitsInByte - 1;
+
+/*
+  Check bit offset
+
+  Internal.
+*/
+TBool CheckBitOffset(
+  TUint_1 BitOffset
+)
+{
+  return (BitOffset <= MaxOffset);
+}
+
+/*
+  Check bit value
+
+  Internal.
+*/
+TBool CheckBitValue(
+  TUint_1 BitValue
+)
+{
+  return (BitValue <= 1);
+}
 
 /*
   Get bit in byte
-
-  Most callers want to see this function as returning bit value,
-  not execution status. Most callers are sure they are passing
-  valid bit offset.
-
-  If invalid bit offset is passed, we return false. So there is
-  clash between execution status and return value. But I think
-  callers will debug their code.
 */
 TBool me_Bits::GetBit(
+  TUint_1 * BitValue,
   TUint_1 Value,
   TUint_1 Offset
 )
 {
-  if (Offset > MaxOffset)
+  if (!CheckBitOffset(Offset))
     return false;
 
-  TBool Result;
-
-  TUint_1 Mask = (1 << Offset);
-
-  Result = ((Value & Mask) != 0);
-
-  return Result;
-}
-
-/*
-  Set bit in byte
-
-  We're updating value in place.
-
-  Unlike GetBit() we have freedom to return execution status.
-*/
-TBool me_Bits::SetBit(
-  TUint_1 * Value,
-  TUint_1 Offset,
-  TBool BitValue
-)
-{
-  if (Offset > MaxOffset)
-    return false;
-
-  TUint_1 Mask;
-
-  if (BitValue == false)
-  {
-    // Clear bit
-    Mask = ~(1 << Offset);
-    *Value = *Value & Mask;
-  }
-  else if (BitValue == true)
-  {
-    // Set bit
-    Mask = (1 << Offset);
-    *Value = *Value | Mask;
-  }
+  *BitValue = (Value >> Offset) & 1;
 
   return true;
 }
 
 /*
-  2024-10-25
-  2024-10-26
+  Set bit to zero
+*/
+TBool me_Bits::SetBitToZero(
+  TUint_1 * Value,
+  TUint_1 Offset
+)
+{
+  if (!CheckBitOffset(Offset))
+    return false;
+
+  *Value = *Value & (~(1 << Offset));
+
+  return true;
+}
+
+/*
+  Set bit to one
+*/
+TBool me_Bits::SetBitToOne(
+  TUint_1 * Value,
+  TUint_1 Offset
+)
+{
+  if (!CheckBitOffset(Offset))
+    return false;
+
+  *Value = *Value | (1 << Offset);
+
+  return true;
+}
+
+/*
+  Set bit in byte
+*/
+TBool me_Bits::SetBit(
+  TUint_1 * Value,
+  TUint_1 Offset,
+  TUint_1 BitValue
+)
+{
+  if (!CheckBitValue(BitValue))
+    return false;
+
+  if (BitValue == 1)
+    return SetBitToOne(Value, Offset);
+  else
+    return SetBitToZero(Value, Offset);
+}
+
+/*
+  2024-10 # #
+  2025-07-29
 */
--- src/me_Bits.h
+++ src/me_Bits.h
@@ -2,30 +2,47 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-10-26
+  Last mod.: 2025-07-29
 */
 
 #pragma once
 
 #include <me_BaseTypes.h>
 
+/*
+  Getting/setting bit in byte
+*/
 namespace me_Bits
 {
-  // Get bit in byte
+  // Get bit
   TBool GetBit(
+    TUint_1 * BitValue,
     TUint_1 Value,
     TUint_1 Offset
   );
 
-  // Set bit in byte
+  // Set bit to zero
+  TBool SetBitToZero(
+    TUint_1 * Value,
+    TUint_1 Offset
+  );
+
+  // Set bit to one
+  TBool SetBitToOne(
+    TUint_1 * Value,
+    TUint_1 Offset
+  );
+
+  // Set bit to value
   TBool SetBit(
     TUint_1 * Value,
     TUint_1 Offset,
-    TBool BitValue
+    TUint_1 BitValue
   );
 }
 
 /*
   2024-10-25
   2024-10-26
+  2025-07-29
 */
