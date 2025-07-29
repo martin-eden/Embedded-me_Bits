2471b7eec5120373edd7cb250e794407b49835ee
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2025-07-29 13:05:38 +0200

    [+] [Example] Example is no longer dummy.

--- examples/me_Bits/me_Bits.ino
+++ examples/me_Bits/me_Bits.ino
@@ -2,7 +2,7 @@
 
 /*
   Author: Martin Eden
-  Last mod.: 2024-12-19
+  Last mod.: 2025-07-29
 */
 
 #include <me_Bits.h>
@@ -10,6 +10,89 @@
 #include <me_BaseTypes.h>
 #include <me_Uart.h>
 #include <me_Console.h>
+
+void PrintByte(
+  TUint_1 Byte
+)
+{
+  Console.Write("Byte is");
+  Console.Print(Byte);
+  Console.EndLine();
+}
+
+void PrintBit(
+  TUint_1 BitOffset,
+  TUint_1 Byte
+)
+{
+  using
+    me_Bits::GetBit;
+
+  Console.Write("Bit");
+  Console.Print(BitOffset);
+  Console.Write("is ");
+
+  if (GetBit(Byte, BitOffset))
+    Console.Write("1");
+  else
+    Console.Write("0");
+
+  Console.EndLine();
+}
+
+void RunTest()
+{
+  using
+    me_Bits::SetBit;
+
+  TUint_1 Byte;
+
+  // Filling 0x00 with ones
+  {
+    Console.Print("( Filling 0x00 with ones");
+
+    Byte = 0;
+
+    PrintByte(Byte);
+
+    for (TUint_1 Offset = 0; Offset < 8; ++Offset)
+    {
+      SetBit(&Byte, Offset, true);
+      PrintByte(Byte);
+    }
+
+    Console.Print(")");
+  }
+
+  // Filling 0xFF with zeroes
+  {
+    Console.Print("( Filling 0xFF with zeroes");
+
+    Byte = 0xFF;
+
+    PrintByte(Byte);
+
+    for (TUint_1 Offset = 0; Offset < 8; ++Offset)
+    {
+      SetBit(&Byte, Offset, false);
+      PrintByte(Byte);
+    }
+
+    Console.Print(")");
+  }
+
+  // Getting bits of some byte value
+  {
+    Console.Print("( Getting bits of byte 01101001");
+
+    Byte = B01101001;
+
+    for (TSint_1 Offset = 7; Offset >= 0; --Offset)
+      PrintBit(Offset, Byte);
+
+    Console.Print(")");
+  }
+}
 
 void setup()
 {
@@ -23,12 +106,7 @@ void loop()
 {
 }
 
-// --
-
-void RunTest()
-{
-}
-
 /*
   2024-10-25
+  2025-07-29
 */
